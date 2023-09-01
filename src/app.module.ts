0727891c7e90b4934898d23b403c61807237597b
Mikail <hagamura@mail.ru>
2023-09-01 16:33:45 +0300

    backend trying to deploy commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { CatController } from './cat/cat.controller';
-import { PostController } from './post/post.controller';
-import { UserController } from './user/user.controller';
-import { TypesController } from './types/types.controller';
 import { CatModule } from './cat/cat.module';
 import { PostModule } from './post/post.module';
 import { TypesModule } from './types/types.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user';
+import { Types } from './types/types';
+import { Post } from './post/post';
+import { Cat } from './cat/cat';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: +configService.get<number>('PG_PORT'),
+        username: configService.get('PG_USER'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DATABASE'),
+        schema: configService.get('PG_SCHEMA'),
+        entities: [User, Types, Post, Cat],
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot(),
     CatModule,
     PostModule,
